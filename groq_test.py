a8a7bdc33587c2abe36634999f3514f5abbe6b47
arahmanmdmajid <arahmanmdmajid@gmail.com>
2025-11-25 20:50:09 +0300

    new repo for TR code and certification

---
+++ groq_test.py
@@ -0,0 +1,25 @@
+import os
+
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+groq_api_key = os.environ.get("GROQ_API_KEY")
+
+
+client = Groq(
+    api_key=groq_api_key,
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+)
+
+print(chat_completion.choices[0].message.content)
